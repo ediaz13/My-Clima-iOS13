b33c96144ca42251b7bd55b6dae2d5ba750aa76c
ediaz13 <bbadmin@MBP-de-BBADmin.fibertel.com.ar>
2024-05-06 22:01:15 -0300

    148. Understanding the OpenWeather API and URL Parameters

---
+++ Clima/Model/WeatherManager.swift
@@ -0,0 +1,20 @@
+//
+//  WeatherManager.swift
+//  Clima
+//
+//  Created by ediaz13 on 06/05/2024.
+//  Copyright © 2024 App Brewery. All rights reserved.
+//
+
+import Foundation
+
+struct WeatherManager {
+    
+    
+    let weatherURL = "https://api.openweathermap.org/data/2.5/weather?appid=8df912e684ae3c3de52636af654f42bc"
+    
+    func fetchWeather(cityName: String) {
+        let urlString = "\(weatherURL)&q=\(cityName)"
+        print(urlString)
+    }
+}
